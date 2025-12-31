343c7b29b374e78ad0698985bc44b7d804aed6fe
cris-rr <59718617+cris-rr@users.noreply.github.com>
2025-12-31 16:22:33 -0500

    client Error handling

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -21,26 +21,35 @@ type Message = {
 const ChatBot = () => {
   const [messages, setMessages] = useState<Message[]>([]); // Placeholder for chat messages state
   const [isBotTyping, setIsBotTyping] = useState(false);
-  const formRef = useRef<HTMLFormElement | null>(null);
+  const [error, setError] = useState('');
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
   const conversationId = useRef(crypto.randomUUID()); // Placeholder for conversation ID management
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
   useEffect(() => {
-    formRef.current?.scrollIntoView({ behavior: 'smooth' });
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
   const onSubmit = async ({ prompt }: FormData) => {
-    setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
-    setIsBotTyping(true);
-    reset();
+    try {
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+      setIsBotTyping(true);
+      setError('');
 
-    const { data } = await axios.post<ChatResponse>('/api/chat', {
-      prompt,
-      conversationId: conversationId.current,
-    });
-    console.log(data);
-    setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
-    setIsBotTyping(false);
+      reset({ prompt: '' });
+
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
+        prompt,
+        conversationId: conversationId.current,
+      });
+      console.log(data);
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+    } catch (error) {
+      console.error(error);
+      setError('Something went wrong. Please try again!');
+    } finally {
+      setIsBotTyping(false);
+    }
   };
 
   const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
@@ -59,12 +68,13 @@ const ChatBot = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-col gap-3 mb-10">
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
         {messages.map((message, index) => (
-          <p
+          <div
             key={index}
             onCopy={onCopyMessage}
+            ref={index === messages.length - 1 ? lastMessageRef : null}
             className={`px-3 py-1 rounded-xl ${
               message.role === 'user'
                 ? 'bg-blue-500 text-white self-end'
@@ -72,7 +82,7 @@ const ChatBot = () => {
             }`}
           >
             <ReactMarkdown>{message.content}</ReactMarkdown>
-          </p>
+          </div>
         ))}
         {isBotTyping && (
           <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl">
@@ -81,11 +91,11 @@ const ChatBot = () => {
             <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]"></div>
           </div>
         )}
+        {error && <p className="text-red-500">{error}</p>}
       </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
         onKeyDown={onKeyDown}
-        ref={formRef}
         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
       >
         <textarea
@@ -93,6 +103,7 @@ const ChatBot = () => {
             required: true,
             validate: (data) => data.trim().length > 0,
           })}
+          autoFocus
           className="w-full border-0 focus:outline-0 resize-none"
           placeholder="Ask anything"
           maxLength={1000}
